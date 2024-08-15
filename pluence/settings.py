312f2ec81f3ac9aa2bde22658835f886401999a6
unknown <rahulthesun82@gmail.com>
2024-08-15 22:59:23 +0530

    Debug = False. Gunicorn Crash Bug Fixed , Reason was incorrect requirement

--- pluence/settings.py
+++ pluence/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&62%_%xn@&vv=+0m+d$nd7k85c%0d4foo*@6+w=jgwxzxj2^m7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1","51.20.5.251" , "pluence.in" ,"www.pluence.in", "18.233.97.170"]
 
