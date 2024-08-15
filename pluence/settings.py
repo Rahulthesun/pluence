54313d3bc82f43c07534e3f23b230a26279dccf0
unknown <rahulthesun82@gmail.com>
2024-08-15 21:01:26 +0530

    supabase postgresql database migration

--- pluence/settings.py
+++ pluence/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,6 +99,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES['default'] = dj_database_url.parse('postgresql://postgres.xudrwqjqvgqtafbpadbs:pluence2024@aws-0-us-west-1.pooler.supabase.com:6543/postgres')
 
 
 # Password validation
