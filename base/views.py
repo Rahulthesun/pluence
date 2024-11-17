98695f31c0e27ca1f1bf01da93439b77ed7a97ba
adith-vj <adith3050@gmail.com>
2024-11-17 19:07:54 +0530

    Privacy policy and terms and conditions added

--- base/views.py
+++ base/views.py
@@ -1057,3 +1057,8 @@ class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
         except Exception as e:
             print(f"Error: {e}")
             return False
+def privacy_policy(request):
+    return render(request, 'base/privacypolicy.html')
+
+def terms_and_conditions(request):
+    return render(request, 'base/termsandconditions.html')
