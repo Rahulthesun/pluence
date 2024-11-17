10f514591358da2ff299e95ad60848d9aea2cc9a
unknown <rahulthesun82@gmail.com>
2024-11-18 02:33:05 +0530

    Cummulative Pre-testing Update for Tiktok Integration-2 : invalid grant_type error

--- base/views.py
+++ base/views.py
@@ -1287,7 +1287,8 @@ def tiktok_authorize(request):
             'scope': "user.info.basic,user.info.profile,user.info.stats",
             "state": "some random_state",
             'code_challenge': code_challenge,
-            'code_challenge_method': 'S256'
+            'code_challenge_method': 'S256',
+            'grant_type': 'authorization_code'
         }
         print(authorization_data['redirect_uri'])
         #encoding authorization parameters into the url
